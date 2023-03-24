6889953ef7dd362fad10a6f358065c81fd0347ce
Suvodeep Das <suvodeep568@gmail.com>
2023-03-24 23:12:45 +0530

    Add files via upload

---
+++ linked list/Linked_list_cycle.cpp
@@ -0,0 +1,17 @@
+//using fast and slow pointers
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        while (head==NULL or head->next==NULL)
+            return false;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast->next and fast->next->next){
+        fast = fast->next->next;
+        slow = slow->next;
+        if  (fast==slow)
+            return true;
+        }
+        return false;
+    }
+};
